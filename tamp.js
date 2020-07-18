0732b5572a10c57948b0bf1b4c37ce0ee3f72323
Radosław Kuczyński <krooopel@gmail.com>
2020-07-19 00:23:34 +0200

    samll cleaning

--- tamp.js
+++
@@ -1,5 +0,0 @@
-.populate("categories", "-__v -_id -subCategories -parentCategory")
-.populate("author", "-__v")
-.populate("subCategories", "-__v -_id -subCategories -parentCategory")
-.populate("parentCategory", "-__v -_id -subCategories -parentCategory")
-.select("-__v");
